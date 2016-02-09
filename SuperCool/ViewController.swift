7a6d7b6b962babc0f45a5618fb1b6c2538d09ab4
Rachel Mclennan <Rachel@node-1w7jr9ujtdsei48n0un65jarq.ipv6.telus.net>
2016-02-08 19:21:16 -0700

    Initial Commit

---
+++ SuperCool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by Rachel Mclennan on 2016-02-08.
+//  Copyright © 2016 Mako Industries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
